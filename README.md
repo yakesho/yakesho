d9bfef8b4ca3ae5e1959dc51f709977d07692c77
yakesho <yakesho@outlook.com>
2021-07-26 17:04:30 +0800

    Updates

---
+++ README.md
@@ -0,0 +1,5 @@
+# Portfolio
+Simple portfolio made with HTML, CSS and JS
+
+## License
+This portfolio is licensed under the **MIT License**. Read the full license [here](./LICENSE).
---
+++ index.js
@@ -0,0 +1,6 @@
+particleground(document.getElementById("particles"), {
+  dotColor: "#5cbdaa",
+  lineColor: "#5cbdaa",
+});
+const intro = document.getElementById("intro");
+intro.style.marginTop = -intro.offsetHeight / 2 + "px";
